3a0ab8e86bb0064c2b7e6b286805d6f8e04f8455
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2017-06-25 19:03:21 +0100

    Wrote the tests for the GET model for the API

---
+++ client/src/models/specs/getSpecs.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const Get = require('../get.js');
+
+describe("From the database:", function(){
+
+  let get;
+
+  before(function(){
+    get = new Get();
+  })
+
+  it("Can get all entries", function(){
+
+  });
+
+  it("Can get the first entry", function(){
+
+  });
+
+  it("Can get the last entry", function(){
+
+  });
+
+  it("can get the second entry", function(){
+
+  });
+
+  it("can get all of a catagory", function(){
+
+  });
+
+  it("can get all of a single spoken work", function(){
+
+  });
+
+})
